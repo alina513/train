16f179c2eac980bd3a35526522b3d947ac003a69
alina513 <130853290+alina513@users.noreply.github.com>
2023-09-13 08:45:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ const onClick = () => {
 };
 
 button.addEventListener("click", onClick);
+
+
+
+const date = new Date();
+console.log("Date: ", date);
+
+// Повертає день місяця від 1 до 31
+console.log("getDate(): ", date.getDate());
+
+// Повертає день тижня від 0 до 6
+console.log("getDay(): ", date.getDay());
+
+// Повертає місяць від 0 до 11
+console.log("getMonth(): ", date.getMonth());
+
+// Повертає рік з 4 цифр
+console.log("getFullYear(): ", date.getFullYear());
+
+// Повертає години
+console.log("getHours(): ", date.getHours());
+
+// Повертає хвилини
+console.log("getMinutes(): ", date.getMinutes());
+
+// Повертає секунди
+console.log("getSeconds(): ", date.getSeconds());
+
+// Повертає мілісекунди
+console.log("getMilliseconds(): ", date.getMilliseconds());
